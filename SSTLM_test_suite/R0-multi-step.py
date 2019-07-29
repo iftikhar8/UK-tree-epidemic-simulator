08039db91f66495453c9c1fd51a40b10db9dc86f
J W Holden <johnholden7@gmail.com>
2019-07-29 15:23:12 +0100

    hpc to lcl

--- SSTLM_test_suite/R0-multi-step.py
+++ SSTLM_test_suite/R0-multi-step.py
@@ -12,7 +12,7 @@ This is useful to get:
 in_ = sys.argv[1:]
 job_id, date, time, name = in_
 L = 100                      # lattice size
-R0 = 50                     # number of initial secondary infected cells given rho = 1
+R0 = 10                     # number of initial secondary infected cells given rho = 1
 rho = np.array([0.01])      # tree density
 alpha = 0.005               # lattice constant in km
 real_dispersal = 0.050      # target dispersal in km
@@ -55,7 +55,4 @@ path = os.getcwd() + '/out_data/'
 np.save(path+name, R0_en_arr)
 
 R0_average = R0_en_arr.sum(axis=0) / (repeat + 1)
-plt.plot(R0_average)
-plt.show()
-
 print(R0_en_arr.sum(axis=1))
